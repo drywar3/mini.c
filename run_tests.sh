6c38886677060a2f2040643216cc880b9b793b50
drywar3 <dry.software.code@gmail.com>
2026-09-04 21:34:37 +0100

    (general) added valgrind to tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,10 +1,22 @@
 
+RUN_VALGRIND="no"
+
+for arg in $@; do
+    if [[ $arg == "--valgrind" ]]; then
+        RUN_VALGRIND="yes"
+    fi
+done
+
 for file in $(find ./tests/ -type f -name *.c); do
     echo "====================================================="
     echo "|| $file"
     echo "====================================================="
     if cc $file -Iinclude ./build/libmini.c.a -o $file.test $3; then
-        valgrind $file.test
+        if [[ $RUN_VALGRIND == "yes" ]]; then
+            valgrind $file.test
+        else
+            $file.test
+        fi
         rm $file.test
     else
         echo "test $file failed"
